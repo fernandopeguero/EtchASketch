0b77829a3d7160c83ae5551fbd88d1aaeb3c809c
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-10-08 22:47:22 -0400

    Add function to create grid

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
+let gridSize = 1;
+const containerWidth = 500;
+
 const userInput = document.querySelector(".grid-size");
 
-
-
 userInput.addEventListener("input", (e) => {
-    const value = e.target.value || 0;
-    
+    gridSize = e.target.value || 1;
+
+    createGrid(gridSize);
 })
+
+
+function createGrid(size) {
+
+    const container = document.querySelector(".grid-container");
+
+    const gridWidth = 500 / Number(size);
+
+    container.innerHTML = "";
+
+    const grid = [];
+
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const div = document.createElement("div");
+            div.classList.add("block");
+            div.style.minWidth = gridWidth +"px";
+            container.appendChild(div)
+        }
+    }
+
+}
+
+
+createGrid(1);
+
 
