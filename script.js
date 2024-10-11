395017581683c9893d8f475514c177ba315e6de2
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-10-11 14:21:32 -0400

    Add rainbow button functionality and change NextGrayColor functionality
    The nextGrayColor change to nextColor function now works for any list
    that you give it it return the current color at the position.

--- script.js
+++ script.js
@@ -4,8 +4,31 @@ const containerWidth = 500;
 const userInput = document.querySelector(".grid-size");
 
 let currentColor = "black";
-const currentGrayColor = nextGrayColor();
-const grayScale = ["#e5e6e5", "#c9cac9", "#aeafae", "#939493", "#797a79", "#616161", "#494a49", "#323332", "#1d1e1d", "#030503"];
+
+const grayScale = [
+    "#e5e6e5",
+    "#c9cac9",
+    "#aeafae",
+    "#939493",
+    "#797a79",
+    "#616161",
+    "#494a49",
+    "#323332",
+    "#1d1e1d",
+    "#030503",
+];
+const rainbowList = [
+    "#9400D3",
+    "#4B0082",
+    "#0000FF",
+    "#00FF00",
+    "#FFFF00",
+    "#FF7F00",
+    "#FF0000",
+];
+
+const currentGrayColor = nextColor(grayScale);
+const currentRainbowColor = nextColor(rainbowList);
 
 const buttonControler = document.querySelector(".buttons-controler");
 
@@ -13,11 +36,9 @@ userInput.addEventListener("input", (e) => {
     gridSize = e.target.value || 1;
 
     createGrid(gridSize);
-})
-
+});
 
 function createGrid(size) {
-    
     gridSize = Number(size);
 
     const container = document.querySelector(".grid-container");
@@ -30,66 +51,59 @@ function createGrid(size) {
         for (let j = 0; j < size; j++) {
             const div = document.createElement("div");
             div.classList.add("block");
-            div.style.minWidth = gridWidth +"px";
+            div.style.minWidth = gridWidth + "px";
 
-            div.addEventListener("mouseover" , function (e) {
-
-                if(currentColor === "black"){
+            div.addEventListener("mouseover", function (e) {
+                if (currentColor === "black") {
                     this.style.backgroundColor = "black";
                 } else if (currentColor === "erase") {
                     this.style.backgroundColor = "white";
-                } else if (currentColor === "gray-scale"){
+                } else if (currentColor === "gray-scale") {
                     this.style.backgroundColor = currentGrayColor();
+                } else if (currentColor === "rainbow") {
+                    this.style.backgroundColor = currentRainbowColor();
                 }
-                
-            })
-            container.appendChild(div)
+            });
+            container.appendChild(div);
         }
     }
 }
 
 createGrid(1);
 
-
 function reset() {
-    if(gridSize > 1){
+    if (gridSize > 1) {
         createGrid(gridSize);
     } else {
         userInput.value = 1;
-    gridSize = 1;
-    createGrid(1);
+        gridSize = 1;
+        createGrid(1);
     }
-    
 }
 
-function nextGrayColor () {
-    let counter = 0 ;
+function nextColor(list) {
+    let counter = 0;
 
     return () => {
-        if(counter === grayScale.length - 1) {
+        if (counter === list.length - 1) {
             counter = 0;
         } else {
-            counter++
+            counter++;
         }
-        return grayScale[counter];
-    }
+        return list[counter];
+    };
 }
 
-// Buttons event listeners 
+// Buttons event listeners
 
-buttonControler.addEventListener("click" , (e) => {
-
+buttonControler.addEventListener("click", (e) => {
     const target = e.target.classList[0];
 
-    switch(target) {
+    switch (target) {
         case "reset":
             reset();
             break;
-        default :
+        default:
             currentColor = e.target.classList[0];
     }
-    
-})
-
-
-
+});
